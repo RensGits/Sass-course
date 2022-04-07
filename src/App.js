dbcf6869be031c172dcdfd33f5f8007ab4e3c981
Rens van Driel <rens.van.a.driel@sogeti.com>
2022-04-07 15:06:05 +0200

    Sass mixins with params

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">About us</a></li>
-          <li><a href="">Services</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
-      </nav>
+      <div className="navigation-container">
+        <nav className="nav">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      
       <section>
         <h1>Header</h1>
-        <h2>Subheader</h2>
+        <h3>Subheader</h3>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium eos autem dicta impedit. Ipsa ex ipsum deserunt ad excepturi possimus amet pariatur quas, tenetur distinctio odio vitae tempore dignissimos consequuntur ullam. Illo voluptates, ullam commodi aliquid nulla ad ducimus, reprehenderit dolores sapiente dicta nihil obcaecati, facere accusantium quia eos. Assumenda, mollitia velit beatae deserunt accusantium asperiores amet quam eos quod corporis, dolore repudiandae, labore sed a aspernatur fugit suscipit! Laudantium eos veniam nulla odit et ratione at ipsum animi expedita doloremque. Perspiciatis nihil tempora ratione ullam facere sapiente, mollitia, nesciunt assumenda necessitatibus accusamus similique placeat laudantium. Hic tenetur, sunt optio sit culpa laudantium adipisci cumque unde voluptas temporibus molestiae similique natus saepe! Quis omnis sed vero ut reprehenderit, labore quae quos nemo nam. Ullam sint optio consectetur. Repudiandae provident unde dignissimos quisquam quis, eum temporibus soluta debitis aspernatur mollitia. Natus recusandae id quod temporibus voluptatum, quae culpa ipsa dolores ex, sunt minima vero accusamus cumque aut sit modi doloribus saepe commodi, nesciunt explicabo eum. Numquam placeat et eveniet laudantium, rem aliquid tempora nam non expedita. In ex quos distinctio quis maxime voluptatibus vero, amet inventore iste blanditiis quas sed rerum, repellendus adipisci sunt impedit voluptates praesentium ab dignissimos, tempore eveniet.</p>
-        <h2>Subheader</h2>
+        <h3>Subheader</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptatum similique delectus ullam excepturi aliquid necessitatibus quidem quod! Ipsa voluptas voluptate consequuntur voluptates molestiae magni natus doloribus inventore sit. Reiciendis necessitatibus, quidem sint ea consequuntur vero natus iure laudantium sapiente numquam corporis ullam et expedita praesentium aperiam vitae veritatis quia?</p>
       </section>
       <footer>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, sint.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, et.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, molestias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, sint.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, molestias.</p>
       </footer>
     </div>
   );
