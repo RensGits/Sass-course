c65d2e0634b0317a6889be1d35704cb36cc09d10
Rens van Driel <rens.van.a.driel@sogeti.com>
2022-04-08 14:58:28 +0200

    scss folder restructure + css changes

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import '../scss/components/_footer.scss'
+
+export default function Footer(){
+    return(
+        <footer className='footer-container'>
+            <ul className="footerlinks-container">
+                <li className="footerlink">Home</li>
+                <li className="footerlink">About us</li>
+                <li className="footerlink">Services</li>
+                <li className="footerlink">Contact</li>
+            </ul>
+            <ul className="footerlinks-container">
+                <li className="footerlink">Home</li>
+                <li className="footerlink">About us</li>
+                <li className="footerlink">Services</li>
+                <li className="footerlink">Contact</li>
+            </ul>
+            <ul className="footerlinks-container">
+                <li className="footerlink">Home</li>
+                <li className="footerlink">About us</li>
+                <li className="footerlink">Services</li>
+                <li className="footerlink">Contact</li>
+            </ul>
+        </footer>
+    )
+}
+
